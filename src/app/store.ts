d2848b78628b6d6e4e62b549e3dbeb2141ee3ab3
Alexander Ziskind <alex@nuvious.com>
2017-08-17 07:50:22 -0400

    distinct on select

--- src/app/store.ts
+++ src/app/store.ts
@@ -8,31 +8,16 @@ import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { State, INITIAL_STATE } from "./shared/models/state";
 
 export class Store {
-
     private subj = new BehaviorSubject<State>(INITIAL_STATE);
-    //private store = this.subj.asObservable().distinctUntilChanged();
-    //public prop1Store = this.subj.asObservable().distinctUntilChanged((x: State, y: State) => x.prop1 === y.prop1);
 
-    //private uiState2 = this.subj.pluck<State, string>('prop1').distinctUntilChanged<string>((x) => { return x.value; });
-
-    constructor() {
-        /*
-        this.store
-            .subscribe(newState => {
-                console.log('new state received');
-            });
-            */
+    public select<T>(name: string): Observable<T> {
+        return this.subj.pluck<State, T>(name).distinctUntilChanged<T>();
     }
 
     get value() {
         return this.subj.value;
     }
 
-    public select<T>(name: string): Observable<T> {
-        //return this.store.pluck<State, T>(name).distinctUntilChanged<T>((x: T, y: T) => x[name] === y[name]);
-        //return this.store.pluck<State, T>(name);
-        return this.subj.pluck<State, T>(name).distinctUntilChanged<T>();
-    }
 
     public set<T>(name: string, state: T) {
         this.subj.next({
